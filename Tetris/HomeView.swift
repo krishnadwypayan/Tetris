d6f6a7cc005c445185c801c43841a0f7c9667ca7
Krishna Kota <krishnadwypayan12@gmail.com>
2024-01-31 00:46:03 +0530

    Initial Commit

---
+++ Tetris/HomeView.swift
@@ -0,0 +1,76 @@
+//
+//  WelcomeView.swift
+//  Tetris
+//
+//  Created by krishna dwypayan on 31/01/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            NameView()
+            Spacer()
+            MenuView()
+            Spacer()
+        }.padding()
+    }
+}
+
+struct NameView: View {
+    var body: some View {
+        HStack {
+            Text("TETRIS")
+                .font(.system(size: 60, weight: .bold, design: .rounded))
+                .multilineTextAlignment(.center)
+                .foregroundStyle(
+                    LinearGradient(
+                        colors: [.purple, .indigo, .blue, .green, .yellow, .orange, .red], startPoint: .leading, endPoint: .trailing
+                        )
+                    )
+                .shadow(radius: 3)
+        }
+    }
+}
+
+struct MenuView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Button {
+                // Implement New Game
+            } label: {
+                Text("New Game")
+                    .font(.title3).bold()
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
+            
+            Button {
+                // Implement
+            } label: {
+                Text("High Scores")
+                    .font(.title3).bold()
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.bordered)
+            .controlSize(.large)
+            
+            Button {
+                // Implement
+            } label: {
+                Text("Menu")
+                    .font(.title3).bold()
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.bordered)
+            .controlSize(.large)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Tetris/TetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetrisApp.swift
+//  Tetris
+//
+//  Created by krishna dwypayan on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
